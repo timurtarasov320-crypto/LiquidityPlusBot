b1e1d8016111a97f7f0222cac974c94fa3c84300
timurtarasov320-crypto <timurtarasov320@gmail.com>
2026-07-17 11:57:50 +0300

    Move config to environment variables

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-TOKEN = "8931692248:AAHUKuXF7x03ZJK8ZqalybI26vrM1-AZ1vE"
-ADMIN_ID = 5681851735
+import os
 
-CRYPTO_PAY_TOKEN = "609638:AAG8HtdebxDT4MlBoaVXtCfMXad3DB5Otry"
+TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "5681851735"))
 
-# Для реальных платежей:
-CRYPTO_PAY_API_URL = "https://pay.crypt.bot/api"
-
-# Для тестовой версии используй вместо строки выше:
-# CRYPTO_PAY_API_URL = "https://testnet-pay.crypt.bot/api"
+CRYPTO_PAY_TOKEN = os.getenv("CRYPTO_PAY_TOKEN")
+CRYPTO_PAY_API_URL = os.getenv(
+    "CRYPTO_PAY_API_URL",
+    "https://pay.crypt.bot/api"
+)
 
 CHANNELS = [
     "@liquidityplus",
@@ -15,5 +15,7 @@ CHANNELS = [
     "@skytraded",
 ]
 
-# HTTPS address of the Telegram Mini App
-WEBAPP_URL = "https://YOUR-DOMAIN.example"
+WEBAPP_URL = os.getenv(
+    "WEBAPP_URL",
+    "https://liquidityplusbot.onrender.com"
+)
